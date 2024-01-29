830bd6319e2d495afa664019704f5aa96d93e75f
kyng-cytro <johndibashi2001@gmail.com>
2024-01-29 01:34:32 +0100

    feat: implement search and start commands

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,3 +1,52 @@
+import { homeLayout } from "../responses/button-layouts";
+import { searchResultKeyboard } from "../responses/inline-keyboards";
 import { GrammyContext } from "../types";
 
-export const searchCallBack = async (ctx: GrammyContext) => {};
+export const searchCallBack = async (ctx: GrammyContext) => {
+  const query = ctx.match;
+
+  if (!query)
+    return await ctx.reply(
+      "🤔 You didn't pass in a valid TV-Show (search <name of tv-show>)",
+      { reply_markup: homeLayout },
+    );
+
+  const msg = await ctx.reply("⌛ Hold on we are fetching results...", {
+    reply_parameters: { message_id: ctx.message?.message_id! },
+  });
+
+  // TODO: get real data from scrape
+  const results = [
+    {
+      show_id: "",
+      image:
+        "https://earlystemer.com/wp-content/uploads/2021/07/earlystemer-logo-120x46.png",
+      name: "test",
+      summary: "test",
+      link: "wdw",
+    },
+  ];
+
+  if (!results.length)
+    return await ctx.api.editMessageText(
+      ctx.user_id!,
+      msg.message_id,
+      `No Shows found for the name *${query}*`,
+      { parse_mode: "Markdown" },
+    );
+
+  await ctx.api.editMessageText(
+    ctx.user_id!,
+    msg.message_id,
+    `Here are the top ${results.length} results for *${query}*`,
+    { parse_mode: "Markdown" },
+  );
+
+  for (const item of results) {
+    await ctx.replyWithPhoto(item.image, {
+      caption: `*${item.name}*\n\n${item.summary}\n\n[👀 Read More](${item.link})`,
+      parse_mode: "Markdown",
+      reply_markup: searchResultKeyboard(item.show_id),
+    });
+  }
+};
--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,3 +1,25 @@
+import { homeLayout } from "../responses/button-layouts";
 import { GrammyContext } from "../types";
+import { addUser, getUser } from "../utils/database";
+import { randomGreeting } from "../utils/helpers";
 
-export const startCallBack = async (ctx: GrammyContext) => {};
+export const startCallBack = async (ctx: GrammyContext) => {
+  if (ctx.user)
+    return await ctx.reply(
+      `*${randomGreeting()} ${ctx.first_name}* Welcome back to Screen Time 🍿`,
+      {
+        parse_mode: "Markdown",
+        reply_markup: homeLayout,
+      },
+    );
+
+  await addUser({ userId: ctx.user_id!, name: ctx.first_name! });
+
+  return await ctx.reply(
+    `*${randomGreeting()} ${ctx.first_name}* Welcome to Screen Time 🍿`,
+    {
+      parse_mode: "Markdown",
+      reply_markup: homeLayout,
+    },
+  );
+};
