1c47fc331f5a78db33cd542ac076e591a485311b
Kyng Cytro <johndibashi2001@gamil.com>
2024-01-30 04:36:59 +0100

    chore: restore previous addition

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,4 +3,7 @@ import { bot } from "../src/bot";
 const { VERCEL_URL: host, WEBHOOK: webhook = `https://${host}/api/webhook` } =
   process.env;
 
+// Try removing first
+void bot.api.deleteWebhook();
+
 void bot.api.setWebhook(webhook);
