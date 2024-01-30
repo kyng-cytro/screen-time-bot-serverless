a64ea3cf161ea2462f87a36117b3c4548d7f7155
Kyng Cytro <johndibashi2001@gamil.com>
2024-01-30 04:18:43 +0100

    feat: introduce new types and complete database service

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,15 @@
 import type { Context } from "grammy";
-import type { User } from "../prisma/generated/client";
+import type { Movie, Prisma, Show } from "../prisma/generated/client";
 
 export interface GrammyContext extends Context {
   first_name?: string;
   user_name?: string;
   user_id?: number;
-  user?: User | null;
+  user?: BotUser;
 }
+
+export type BotUser = Prisma.UserGetPayload<{ include: { account: true } }>;
+
+export type Movies = Movie[];
+
+export type Shows = Show[];
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "../../prisma/generated/client/";
+import { PrismaClient, User } from "../../prisma/generated/client/";
+import { Movies, Shows } from "../types";
 
 const prisma = new PrismaClient();
 
@@ -6,6 +7,7 @@ export const getUser = async ({ userId }: { userId: number }) => {
   try {
     return await prisma.user.findUnique({
       where: { userId },
+      include: { account: true },
     });
   } catch (err) {
     console.error(err);
@@ -19,6 +21,22 @@ export const getUsersWithSeriesSub = async () => {
     });
   } catch (err) {
     console.error(err);
+  }
+};
+
+export const getMovies = async () => {
+  try {
+    return await prisma.movie.findMany({});
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getShows = async () => {
+  try {
+    return await prisma.show.findMany({});
+  } catch (err) {
+    console.log(err);
   }
 };
 
@@ -38,4 +56,125 @@ export const addUser = async ({
   }
 };
 
-// TODO: implement rest of db functions
+export const addMovies = async ({ movies }: { movies: Movies }) => {
+  try {
+    return await prisma.$transaction([
+      prisma.movie.deleteMany({}),
+      prisma.movie.createMany({ data: movies, skipDuplicates: true }),
+    ]);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const addShows = async ({ shows }: { shows: Shows }) => {
+  try {
+    return await prisma.$transaction([
+      prisma.show.deleteMany({}),
+      prisma.show.createMany({ data: shows, skipDuplicates: true }),
+    ]);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const subscribeUser = async (
+  props:
+    | { type: "reg"; id: string }
+    | { id: string; type: "custom"; accountId: string; kValue: string },
+) => {
+  try {
+    if (props.type === "reg") {
+      return await prisma.user.update({
+        where: { id: props.id },
+        data: {
+          seriesSub: true,
+        },
+      });
+    }
+
+    return await prisma.user.update({
+      where: { id: props.id },
+      data: {
+        seriesSub: true,
+        custom: true,
+        account: {
+          upsert: {
+            create: {
+              accountId: props.accountId,
+              kValue: props.kValue,
+            },
+            update: {},
+          },
+        },
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const unSubscribeUser = async ({ id }: { id: string }) => {
+  try {
+    return await prisma.user.update({
+      where: { id },
+      data: { seriesSub: false, custom: false },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getUserFollowings = async ({ id }: { id: string }) => {
+  try {
+    return await prisma.followingItem.findMany({
+      where: { users: { some: { id } } },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const addToFollowings = async ({
+  id,
+  showId,
+  showName,
+}: {
+  id: string;
+  showId: string;
+  showName: string;
+}) => {
+  try {
+    return await prisma.user.update({
+      where: { id },
+      data: {
+        following: {
+          connectOrCreate: {
+            where: { itemId: showId },
+            create: { itemId: showId, name: showName },
+          },
+        },
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const checkIfUserIsFollowing = async ({
+  id,
+  showId,
+}: {
+  id: string;
+  showId: string;
+}) => {
+  try {
+    await prisma.followingItem.findFirstOrThrow({
+      where: { id: showId, users: { some: { id } } },
+    });
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
