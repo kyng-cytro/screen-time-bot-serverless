91782ae0155c799c9d5bab959e4c27fe4dbfd614
kyng-cytro <johndibashi2001@gmail.com>
2024-01-30 04:41:26 +0100

    chore: testing deployement when webhook not set

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,7 +3,4 @@ import { bot } from "../src/bot";
 const { VERCEL_URL: host, WEBHOOK: webhook = `https://${host}/api/webhook` } =
   process.env;
 
-// Try removing first
-void bot.api.deleteWebhook();
-
-void bot.api.setWebhook(webhook);
+//void bot.api.setWebhook(webhook);
