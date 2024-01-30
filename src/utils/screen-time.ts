ce7b1bdf4c44aa0d46a670dba5c94ced8e217c0b
Kyng Cytro <johndibashi2001@gamil.com>
2024-01-30 04:17:54 +0100

    feat: complete screen time service

--- src/utils/screen-time.ts
+++ src/utils/screen-time.ts
@@ -1,4 +1,8 @@
-export const create_account = async ({ userId }: { userId: string }) => {
+export const createScreenTimeAccount = async ({
+  userId,
+}: {
+  userId: number;
+}) => {
   try {
     const account = `screen_${userId}`;
     const url = new URL("https://next-episode.net/PAGES/misc/signup.php");
@@ -14,49 +18,67 @@ export const create_account = async ({ userId }: { userId: string }) => {
       console.error("Error creating user account");
       return { account_id: null, k_value: null };
     }
-    return await get_account_info({ userId });
+    return await getScreenTimeAccountInfo({ userId });
   } catch (err) {
     console.error("Error creating user account:", err);
     return { account_id: null, k_value: null };
   }
 };
 
-export const get_account_info = async ({ userId }: { userId: string }) => {
-  const login_info = {
-    username: `screen_${userId}`,
-    password: `screen_${userId}`,
-  };
-
-  const loginResponse = await fetch("https://next-episode.net/userlogin", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams(login_info),
-  });
-
-  if (!loginResponse.ok) {
-    console.error("Login failed");
-    return { account_id: null, k_value: null };
-  }
-
-  const pageResponse = await fetch("https://next-episode.net/userlogin", {});
-
-  if (!pageResponse.ok) {
-    console.error("Failed to fetch user page");
-    return { account_id: null, k_value: null };
-  }
-
-  const pageText = await pageResponse.text();
-
+export const getScreenTimeAccountInfo = async ({
+  userId,
+}: {
+  userId: number;
+}) => {
   try {
+    const login_info = {
+      username: `screen_${userId}`,
+      password: `screen_${userId}`,
+    };
+
+    const loginResponse = await fetch("https://next-episode.net/userlogin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Cookie: "PHPSESSID=ffa218c4b34cac302da2cb93b71b0021;",
+      },
+      body: new URLSearchParams(login_info),
+    });
+
+    if (!loginResponse.ok) {
+      console.error("Login failed");
+      return { account_id: null, k_value: null };
+    }
+
+    const pageText = await loginResponse.text();
+
     const account_id = pageText.match(/user_id=(\d*)/)?.[1] || null;
-    const k_value = pageText.match(/k=(.{32})/)?.[1] || null;
+    const k_value = pageText.match(/&k=(.{32})/)?.[1] || null;
     return { account_id, k_value };
   } catch (err) {
-    console.error("Error parsing page content:", err);
+    console.error("Error grabbing account info:", err);
     return { account_id: null, k_value: null };
   }
 };
 
-console.log(get_account_info({ userId: "" }));
+export const toggleScreenTimeWatchList = async ({
+  accountId,
+  kValue,
+  showId,
+}: {
+  accountId: string;
+  kValue: string;
+  showId: string;
+}) => {
+  try {
+    const url = new URL(
+      "https://next-episode.net/PAGES/misc/toggle_watchlist.inc.php",
+    );
+    url.searchParams.append("k", kValue);
+    url.searchParams.append("show_id", showId);
+    url.searchParams.append("user_id", accountId);
+    fetch(url);
+  } catch (err) {
+    console.error("An error occured toggling that show:", err);
+  }
+};
