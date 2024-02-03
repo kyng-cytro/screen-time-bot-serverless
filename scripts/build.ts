a53ab0f5a9d651a6425b229511877be43e1347c5
kyng-cytro <johndibashi2001@gmail.com>
2024-02-03 10:45:55 +0100

    chore: update build script changes

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,4 +3,6 @@ import { bot } from "../src/bot";
 const { VERCEL_URL: host, WEBHOOK: webhook = `https://${host}/api/webhook` } =
   process.env;
 
+void bot.api.deleteWebhook();
+
 void bot.api.setWebhook(webhook);
