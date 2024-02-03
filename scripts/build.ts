a039a774a0adfa55d3f0832d9492c9ed46062c21
Kyng Cytro <johndibashi2001@gamil.com>
2024-02-03 10:37:32 +0100

    chore: reintegrate build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,4 +3,4 @@ import { bot } from "../src/bot";
 const { VERCEL_URL: host, WEBHOOK: webhook = `https://${host}/api/webhook` } =
   process.env;
 
-//void bot.api.setWebhook(webhook);
+void bot.api.setWebhook(webhook);
