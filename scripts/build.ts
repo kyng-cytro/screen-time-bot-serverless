80186b94e74d0deab7ab01a4fdb3adc96d41e698
kyng-cytro <johndibashi2001@gmail.com>
2024-02-03 10:55:55 +0100

    chore: removed build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,4 +4,4 @@ const { VERCEL_URL: host, WEBHOOK: webhook = `https://${host}/api/webhook` } =
   process.env;
 
 // void bot.api.setWebhook(webhook);
-void bot.api.deleteWebhook();
+// void bot.api.deleteWebhook();
