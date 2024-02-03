07916f38c7c2e01db6b32166562e2e8f037a0b5b
kyng-cytro <johndibashi2001@gmail.com>
2024-02-03 12:07:38 +0100

    feat: update Following type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 import type { Context } from "grammy";
 import { HydrateFlavor } from "@grammyjs/hydrate";
-import type { Prisma, Show } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
 
 export interface GrammyContext extends HydrateFlavor<Context> {
   first_name?: string;
@@ -13,5 +13,6 @@ export type Shows = CreateShow[];
 export type Movies = CreateMovie[];
 export type CreateMovie = Prisma.MovieCreateInput;
 export type CreateShow = Prisma.ShowCreateInput;
-export type SearchResult = Omit<Show, "date" | "createdAt" | "updatedAt">;
+export type Followings = Prisma.FollowingItemCreateInput[];
+export type SearchResult = Omit<CreateShow, "date">;
 export type BotUser = Prisma.UserGetPayload<{ include: { account: true } }>;
